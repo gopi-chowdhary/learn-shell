598e47375431df2f8a8710dec5eac62aa915407f
gopi chowdhary <gopichowdhary99@gmail.com>
2023-09-30 14:53:35 +0530

    variable

--- 03-memory-cal.sh
+++ 03-memory-cal.sh
@@ -2,4 +2,4 @@ total=$(free | grep Mem | awk '{print $2}')
 free=$(free  | grep Men | awk '{print $4}')
 
 free_percentage=$(echo "$free / $total * 100" | bc )
-echo$free_percentage
+echo $free_percentage
